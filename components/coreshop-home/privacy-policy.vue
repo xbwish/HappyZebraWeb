100ff3b21fe28ade0598a35b67e3053e09eb24e0
leslie <xbwish@gmail.com>
2024-08-04 07:24:24 +0800

    feat: 首页底部增加隐私条款

---
+++ components/coreshop-home/privacy-policy.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer">
+    <a href="/agreement">{{ $t("服务条款") }}</a>
+    <a href="/privacyPolicy">{{ $t("隐私政策") }}</a>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer::v-deep {
+  display: flex;
+  justify-content: center;
+  a {
+    font-size: 12px;
+    margin-right: 12px;
+  }
+}
+</style>
